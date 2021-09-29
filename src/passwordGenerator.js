45c5b98dc15b61d0a5ab214fcaddfb46a5be0d00
bryanNativel <bryan.nativel@hotmail.fr>
2021-09-29 16:44:13 +0200

    work

--- src/passwordGenerator.js
+++ src/passwordGenerator.js
@@ -4,47 +4,88 @@ let numberCaractereSpe;
 let numberMaj;
 let numberNumber;
 
-function passwordGeneratore(){
+
+document.querySelectorAll('.form-range').forEach(item => {
+    item.addEventListener('click', event => {
+
+        let l = document.getElementById("lettre").value;
+        let m = document.getElementById("maj").value;
+        let n = document.getElementById("number").value;
+        let s = document.getElementById("caractereSpe").value;
+
+        if (l == 2 && m == 2 && n == 2 && s == 2) {
+            document.getElementById("picturePasswordGenerator").src = "../image/5.png";
+            document.getElementById("lvl").innerHTML = "Mot de passe niveau 1"
+        } else if (l > 2 && l <= 4 && m > 2 && m <= 4 && n > 2 && n <= 4 && s > 2 && s <= 4) {
+            document.getElementById("picturePasswordGenerator").src = "../image/4.png";
+            document.getElementById("lvl").innerHTML = "Mot de passe niveau 2"
+        } else if (l > 4 && l < 6 && m > 4 && m < 6 && n > 4 && n < 6 &&  s > 4 && s < 6) {
+            document.getElementById("picturePasswordGenerator").src = "../image/3.png";
+            document.getElementById("lvl").innerHTML = "Mot de passe niveau 3"
+        } else if (l > 6 && l<8 && m > 6 && m <8 && n > 6 && n <8  && s > 6 && s <8 ) {
+            document.getElementById("picturePasswordGenerator").src = "../image/2.png";
+            document.getElementById("lvl").innerHTML = "Mot de passe niveau 4"
+        }else if (l > 8 && m > 8 && n > 8 && s > 8) {
+            document.getElementById("picturePasswordGenerator").src = "../image/1.png";
+            document.getElementById("lvl").innerHTML = "Mot de passe niveau 5"
+        }
+    })
+})
+
+
+document.getElementById("createPassword").addEventListener("click", function () {
 
     numberLettre = document.getElementById("lettre").value;
     numberLettre > 0 ? lettre(numberLettre) : "";
     numberMaj = document.getElementById("maj").value;
-    numberMaj > 0 ? maj(numberMaj):"";
+    numberMaj > 0 ? maj(numberMaj) : "";
     numberNumber = document.getElementById("number").value;
-    numberNumber > 0 ? number(numberNumber):"";
+    numberNumber > 0 ? number(numberNumber) : "";
     numberCaractereSpe = document.getElementById("caractereSpe").value;
     numberCaractereSpe > 0 ? spe(numberCaractereSpe) : "";
 
-    console.log(password);
-}
+    let response = password;
+    if (response) {
 
-function lettre(num){
+        document.getElementById("passwordResponse").value = response;
+        password = ""
+    }
+});
+
+function lettre(num) {
     let letterR = "abcdefghijklmnopqrstuvwxyz";
-    for (let i = 0; i <= num; i++) {
+    for (let i = 1; i <= num; i++) {
         let randomNumber = Math.floor(Math.random() * letterR.length);
-        password += letterR.substring(randomNumber, randomNumber +1);
+        password += letterR.substring(randomNumber, randomNumber + 1);
     }
 }
 
-function spe(num){
+function spe(num) {
     let speR = "!@#$%^&*()";
-    for (var i = 0; i <= num; i++) {
+    for (var i = 1; i <= num; i++) {
         var randomNumber = Math.floor(Math.random() * speR.length);
-        password += speR.substring(randomNumber, randomNumber +1);
+        password += speR.substring(randomNumber, randomNumber + 1);
     }
 }
 
-function number(num){
+function number(num) {
     let numberR = "0123456789";
-    for (var i = 0; i <= num; i++) {
+    for (var i = 1; i <= num; i++) {
         var randomNumber = Math.floor(Math.random() * numberR.length);
-        password += numberR.substring(randomNumber, randomNumber +1);
+        password += numberR.substring(randomNumber, randomNumber + 1);
     }
 }
-function maj(num){
+
+function maj(num) {
     let majR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (var i = 0; i <= num; i++) {
+    for (var i = 1; i <= num; i++) {
         var randomNumber = Math.floor(Math.random() * majR.length);
-        password += majR.substring(randomNumber, randomNumber +1);
+        password += majR.substring(randomNumber, randomNumber + 1);
     }
+
+    document.getElementById("copyPassword").addEventListener("click", function () {
+        var copyText = document.getElementById("passwordResponse");
+        copyText.select();
+        document.execCommand("copy");
+    });
 }
